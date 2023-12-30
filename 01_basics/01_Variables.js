f4077e98acfa1a39475fbcb139f0986c45f50a7e
Prakash Kumar <65835409+Prakash674@users.noreply.github.com>
2023-12-30 09:33:32 +0000

    var

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail="prakash9572@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=3
+accountEmail="singhprakash9572@gmail.com"
+accountPassword="98765"
+accountCity="Jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
