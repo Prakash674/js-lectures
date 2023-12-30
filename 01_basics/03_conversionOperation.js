c748e38feaa971713b38e2c983580208c7358510
Prakash Kumar <65835409+Prakash674@users.noreply.github.com>
2023-12-30 18:09:56 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score =undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// score="33";
+
+let valueInNumber= Number(score)
+// console.log(valueInNumber)
+// console.log(typeof (valueInNumber));
+
+
+//"33"=> 33
+//"33abc" => NaN
+//true =>1; && false => 0;
+
+
+let isLoggedIn=null
+let booleanIsloggendIn=Boolean (isLoggedIn);
+// console.log(booleanIsloggendIn);
+
+
+//1=> true ,0 =>  false;
+// ""=>false
+//"hello"=>true
+
+// String
+
+let someNumber=33;
+
+let stringSum= String(someNumber);
+console.log(stringSum);
+console.log(typeof stringSum);
