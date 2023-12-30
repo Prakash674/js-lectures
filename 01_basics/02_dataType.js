db411c317f8b2d558b7dfd12cad5721890c138fd
Prakash Kumar <65835409+Prakash674@users.noreply.github.com>
2023-12-30 17:41:54 +0000

    data types

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version means the written code is in new version of js .It is not mandatory to write it  
+
+
+
+
+//number= 2 to power 53
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value
+// undefined
+//symbol= unique
+
+
+
+//object
+console.log(typeof undefined)//undefined
+console.log(typeof null) //object type
